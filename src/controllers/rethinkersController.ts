3d050cc6a0b9cb7cf6ebc49ad3cf7097db117a71
Vinícius Delmo <vinicius.delmo@rethink.dev>
2023-08-02 15:24:04 -0300

    feat: adding photo upload and saving on server

--- src/controllers/rethinkersController.ts
+++ src/controllers/rethinkersController.ts
@@ -1,7 +1,9 @@
 import rethinkersService from '../services/rethinkersService';
 import { Request, Response } from 'express';
+import multer from 'multer';
+import path from 'path';
+import fs from 'fs';
 
-// Controller para cadastrar um novo 'rethinker'
 const registerRethinker = async (req:Request, res:Response ) => {
  
   try {
@@ -20,8 +22,31 @@ const getAllRethinkers = async (req: Request, res: Response) => {
     return res.status(500).json({ error: 'Erro ao obter os rethinkers.' });
   }
 };
+// Configuração do multer para fazer o upload do arquivo
+const uploadPhoto = async (req: any, res: any) => {
+  try {
+    if (!req.file) {
+      return res.status(400).json({ error: 'Image not provided' });
+    }
+    console.log(req.file)
+    const imageName = req.file.filename;
+    console.log(imageName);
 
+    const tempPath = req.file.path;
+    const destinationPath = path.join(__dirname, '..', 'assets', 'profile-images', imageName);
+
+    // Movendo o arquivo temporário para a pasta de destino
+    fs.renameSync(tempPath, destinationPath);
+    console.log(destinationPath);
+
+    return res.json({ message: 'Image uploaded successfully', imageName: imageName });
+  } catch (error) {
+    console.error('Error uploading image:', error);
+    return res.status(500).json({ error: 'Failed to upload image' });
+  }
+};
 export default {
   registerRethinker,
-  getAllRethinkers
+  getAllRethinkers,
+  uploadPhoto
 };
--- src/routes/rethinkersRoute.ts
+++ src/routes/rethinkersRoute.ts
@@ -1,9 +1,26 @@
 import { Router } from 'express';
 import rethinkersController from '../controllers/rethinkersController';
+import multer from 'multer';
+import path from 'path';
 
-const notifications: Router = Router();
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, path.join(__dirname, '..', 'assets', 'profile-images'));
+  },
+  filename: function (req, file, cb) {
+    /* const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9); */
+    cb(null, /* 'profile-image-' + uniqueSuffix + */ file.originalname);
+  },
+});
 
-notifications.post('/insert-rethinker', rethinkersController.registerRethinker);
-notifications.get('/rethinkers', rethinkersController.getAllRethinkers);
+const upload = multer({ storage: storage });
 
-export default notifications;
+const rethinkersRouter = Router();
+
+rethinkersRouter.post('/insert-rethinker', rethinkersController.registerRethinker);
+rethinkersRouter.get('/rethinkers', rethinkersController.getAllRethinkers);
+
+// Use o middleware de upload para a rota /upload-photo
+rethinkersRouter.post('/upload-photo', upload.single('image'), rethinkersController.uploadPhoto);
+
+export default rethinkersRouter;
